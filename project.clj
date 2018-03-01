c40b714c98b7b93f06556c504e217d562dbf77b0
Burin Choomnuan <agilecreativity@gmail.com>
2018-03-01 18:54:30 -0500

    Update project dependencies
    - Rename the included lib to just `andare.jar`
    - Update project dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject git-catch-up "0.1.0"
+(defproject gcu "0.1.1"
   :description "Run git pull for all projects from a given directory using Lumo/ClojureScript"
   :license {:name "The MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojurescript "1.9.854"]
+  :dependencies [[org.clojure/clojurescript "1.10.64"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [andare "0.7.0"]])
+                 [andare "0.9.0"]])
