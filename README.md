fe68771caacf256bf6aa425ce7c322f473d85b6c
Burin Choomnuan <agilecreativity@gmail.com>
2018-03-24 22:08:42 -0400

    Use EPL for license and update ClojureScript
    - Use recent version of ClojureScript
    - Use EPL for license in both
    - Keep just README.org

--- README.md
+++
@@ -1,7 +0,0 @@
-## gcu
-
-Run `git pull` on all projects from a given directory using Lumo/ClojureScript.
-
-## Usage
-
-- See [README](README.org)
--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject gcu "0.1.9"
+(defproject gcu "0.2.1"
   :description "Run git pull for all projects from a given directory using Lumo/ClojureScript"
-  :license {:name "The MIT License"
-            :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojurescript "1.10.64"]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojurescript "1.10.217"]
                  [org.clojure/tools.cli "0.3.5"]
                  [andare "0.9.0"]])
