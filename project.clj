16edc3a317f11806ae0931626d9c67a0e972283b
Burin Choomnuan <agilecreativity@gmail.com>
2018-12-20 15:43:13 -0500

    Bump version to 0.2.3
    - Update project dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject gcu "0.2.2"
+(defproject gcu "0.2.3"
   :description "Run git pull for all projects from a given directory using Lumo/ClojureScript"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojurescript "1.10.238"]
-                 [org.clojure/tools.cli "0.3.5"]
-                 [andare "0.9.0"]])
+  :dependencies [[org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/tools.cli "0.4.1"]
+                 [andare "0.10.0"]])
